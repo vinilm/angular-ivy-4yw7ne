529add79e2948a9ea69e08274d82a9198e865fcd
vinilm <vinilm@yahoo.com>
2021-04-18 09:39:38 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,23 +1,38 @@
 import { Component, Input } from "@angular/core";
+import { NgModel } from "@angular/forms";
 
 @Component({
   selector: "hello",
   template: `
-    <h1>Hello {{ name }}!</h1>
-    <p>
-      <input type="checkbox" #strike />Strike
-      <input type="checkbox" #yellow />Yellow Background
-      <input type="checkbox" #bold />Bold
-    </p>
+    <div [ngClass]="{ bold: isBold, strike: isStrike, highlight: isHighlight }">
+      <h1>Hello {{ name }}!</h1>
+      <p>
+        <input type="checkbox" [(ngModel)]="isBold" />Bold
+        <input type="checkbox" [(ngModel)]="isStrike" />Strike
+        <input type="checkbox" [(ngModel)]="isHighlight" />Highlight
+      </p>
+    </div>
   `,
   styles: [
     `
       h1 {
         font-family: Lato;
       }
+      .bold {
+        font-weight: bold;
+      }
+      .highlight {
+        background-color: cyan;
+      }
+      .strike {
+        text-decoration: line-through;
+      }
     `
   ]
 })
 export class HelloComponent {
   @Input() name: string;
+  isBold = false;
+  isStrike = false;
+  isHighlight = false;
 }
